720a502cc7f90840881de5a54b7cbdc2e042285a
Dey11 <shreyandey11@gmail.com>
2024-04-20 03:43:28 +0530

    Refactor auth.ts to remove unnecessary imports

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,7 +1,5 @@
-import express, { Request, Response } from "express";
+import express from "express";
 
-import User from "../models/user";
-import Post from "../models/post";
 import { login, logout, register } from "../controllers/auth";
 
 export const router = express.Router();
