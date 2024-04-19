d60b80e051944a294e7cc405e3d6c73e16f316b1
Dey11 <shreyandey11@gmail.com>
2024-04-20 03:43:48 +0530

    Add authentication middleware

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { Secret } from "jsonwebtoken";
+
+const validateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as Secret);
+
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    req.body.user = decoded;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
