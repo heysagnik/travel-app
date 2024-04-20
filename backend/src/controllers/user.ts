4ada40040a2a12842cf8e84ecfbaf0bfc8f87e08
Dey11 <shreyandey11@gmail.com>
2024-04-20 06:06:50 +0530

    implement the interested in a trip logic

---
+++ backend/src/controllers/user.ts
@@ -0,0 +1,79 @@
+import { Request, Response } from "express";
+import User from "../models/user";
+import Post from "../models/post";
+
+export const getProfile = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+    const posts = await Post.find({ author: id });
+    res.status(200).json({ user, posts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const closePost = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const userId = req.body.user.id;
+    const post = await Post.findById(id).populate("author", "id").exec();
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    if (post.author.id !== userId) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    post.status = "inactive";
+    await post.save();
+
+    // send notification to all interested users
+
+    res.status(200).json({ message: "Post closed successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const interestedInAPost = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const userId = req.body.user.id;
+    const post = await Post.findById(id)
+      .populate("author", "id")
+      .populate("interestedUsers", "id")
+      .exec();
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    if (post.author.id === userId) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    if (post.status === "inactive") {
+      return res.status(400).json({ message: "Post is closed" });
+    }
+    if (post.interestedUsers.includes(userId)) {
+      return res
+        .status(400)
+        .json({ message: "Already interested in the post" });
+    }
+    post.interestedUsers.push(userId);
+    if (post.limit && post.interestedUsers.length === post.limit) {
+      post.status = "inactive";
+    }
+    await post.save();
+
+    // send notification to the author of the post
+
+    res.status(200).json({ message: "Interested in the post" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+export const router = express.Router();
+
+router.post("/interested/:id", async (req, res) => {});
+
+router.post("/close/:id", async (req, res) => {});
+
+router.get("/profile/:id", async (req, res) => {});
+
+// endpoints not tested yet
