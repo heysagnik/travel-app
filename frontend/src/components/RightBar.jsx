a6944a8946d50d918d16874cafd10e85aacde869
Sagnik Sahoo <sahoosagnik1@gmail.com>
2024-04-20 23:43:46 +0530

    Add NewUsersCard component to RightBar

--- frontend/src/components/RightBar.jsx
+++ frontend/src/components/RightBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import LoginCard from './ui/LoginCard';
+import NewUsersCard from './ui/NewUser';
 
 const RightBar = () => {
+    const isAuthenticated = true;
     return (
         <div className="max-w-348 h-full min-h-screen top-14 bg-white border-l border-grey px-6 py-6 hidden md:block">
-        <LoginCard/>
+        { isAuthenticated ? <NewUsersCard/> : <LoginCard/> }
         </div>
     );
 };
