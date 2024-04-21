ad35c4c8db64f247998c4ee8e58e1971a6fbf531
Sagnik Sahoo <sahoosagnik1@gmail.com>
2024-04-21 12:49:19 +0530

    Add react-toastify package and update FinalScrn component

--- frontend/src/pages/FinalScrn.jsx
+++ frontend/src/pages/FinalScrn.jsx
@@ -1,27 +1,20 @@
 const FinalScrn = () => {
   return (
-    <div>
-      {/* {alert("Contact details sent to the registered mobile number.")} */}
-      <h1 className="text-3xl text-center">A trip to Goa with Anita</h1>
-      <br></br>
-      <p className="text-2xl text-center">
-        Thank you for giving Anita accompany. You will receive an SMS their
-        contact details soon.
+    <div className="flex flex-col items-center space-y-8">
+      <h1 className="text-lg text-center">A trip to Goa with Anita</h1>
+      <p className="text-md text-center">
+        Thank you for giving Anita accompany. You will receive an SMS with
+        their contact details soon.
       </p>
-      <br></br>
-      <br></br>
-      <p className=" text-5xl text-center">Happy Travelling!</p>
-      <br></br>
-      <br></br>
-      <br></br>
-
-      <p className="text-center">
-        <a href="/discover">
-          <button className="bg-slate-200 p-2 rounded-xl">
-            Back to Discover
-          </button>
-        </a>
-      </p>
+      <p className="text-lg text-center">Happy Travelling!</p>
+      <button
+        className="btn btn-primary w-1/2"
+        onClick={() => {
+          document.getElementById("details_sent_modal").close();
+        }}
+      >
+        OK
+      </button>
     </div>
   );
 };
