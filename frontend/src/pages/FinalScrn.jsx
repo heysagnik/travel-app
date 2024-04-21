4bd1faffcbc24416a73014721c0bdbfa94248bc1
Dey11 <shreyandey11@gmail.com>
2024-04-21 19:08:17 +0530

    msg

--- frontend/src/pages/FinalScrn.jsx
+++ frontend/src/pages/FinalScrn.jsx
@@ -10,10 +10,10 @@ const FinalScrn = () => {
 
   return (
     <div className="flex flex-col items-center space-y-8">
-      <h1 className="text-lg text-center">A trip to Goa with Anita</h1>
+      <h1 className="text-xl text-center">Trip</h1>
       <p className="text-md text-center">
-        Thank you for giving Anita accompany. You will receive an SMS with their
-        contact details soon.
+        Thank you for giving the user company. You will receive an SMS with
+        their contact details soon.
       </p>
       <p className="text-lg text-center">Happy Travelling!</p>
       <button
