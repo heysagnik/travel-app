c6533d44809f90cd577a68c406c68a3e87c6e0ee
Dey11 <shreyandey11@gmail.com>
2024-04-21 14:50:17 +0530

    complete profile page

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,27 +2,52 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useRecoilState, useRecoilValue } from "recoil";
-import { authState} from "../recoil/authState";
+import { authState } from "../recoil/authState";
+
+import axios from "axios";
 
 function Profile() {
   const navigate = useNavigate();
   const auth = useRecoilValue(authState);
-  
+
+  const [profile, setProfile] = useState(null);
+
+  const getProfile = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/users/profile/${auth._id}`,
+        {
+          withCredentials: true,
+        }
+      );
+      setProfile(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     if (auth === null || auth === undefined) {
       navigate("/login");
     } else {
-      console.log("Profile mounted");
-      console.log(auth);
-
-    }});
-
+      getProfile();
+    }
+  }, []);
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>{auth.username}</p>
-
+    <div className="m-8">
+      <h3 className=" text-3xl ">Profile</h3>
+      <p className="pt-10">Username: @{auth.username}</p>
+      {profile === null ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <p className="pt-3">Name: {profile.user.name}</p>
+          <p className="pt-3">
+            Number of posts made on TravelMate: {profile.posts.length}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
